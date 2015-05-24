428721aa3b6dcf6356e5cafa36b4bc7a40a06748
soushad <deschryver.dd@gmail.com>
2015-05-25 00:28:22 +0200

    Stylsheets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,16 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( templatemo-style.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( animate.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( FontAwesome.otf)
+Rails.application.config.assets.precompile += %w( fontawesome-webfront.eot)
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.svg)
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.ttf)
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.woff)
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.woff2)
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/info'
 
-  root :to => redirect('/forums')
   resources :forums
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
